8d706df12544a19fc0ec20e14bc6f9bc65ccea30
Juanbar20 <juanbar20.jdb@gmail.com>
2023-02-14 13:13:56 -0500

    Conversor de moneda agregado

---
+++ Proyectos/conversor_de_moneda.py
@@ -0,0 +1,31 @@
+import os
+os.system("cls")
+
+USD = 1
+ARS = 290
+COP = 4600
+MXN = 20
+
+print("Bienvenido al conversor de monedas definitivo")
+print("Elige una de las siguientes opciones de conversión: ")
+print("1. Dolar Estadounidense a pesos argentinos ")
+print("2. Dolar Estadounidense a pesos colombianos ")
+print("3. Dolar Estadounidense a pesos mexicanos ")
+opcion = input("¿Cual es tu opción?: ")
+opcion = int(opcion)
+
+if opcion == 1:
+    
+    dolares = int(input("¿Cuantos dolares tienes?: "))
+    pesos = dolares * ARS
+    print(f"Tienes {dolares} pesos argentinos")
+elif opcion == 2:
+    dolares = int(input("¿Cuantos dolares tienes?: "))
+    pesos = dolares * COP
+    print(f"Tienes {dolares} pesos colombianos")
+elif opcion == 3:
+    dolares = int(input("¿Cuantos dolares tienes?: "))
+    pesos = dolares * MXN
+    print(f"Tienes {dolares} pesos mexicanos")
+else:
+    print("Elige una opción correcta")
