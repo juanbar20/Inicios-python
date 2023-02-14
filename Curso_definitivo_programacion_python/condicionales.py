149ab88076a7ea2b88380c8d06d720b845085971
Juanbar20 <juanbar20.jdb@gmail.com>
2023-02-14 09:50:52 -0500

    Creando juego de piedra papel y tijeras

--- Curso_definitivo_programacion_python/condicionales.py
+++ Curso_definitivo_programacion_python/condicionales.py
@@ -1,6 +1,10 @@
-numero1 = 10
+numero1 = 5
 numero2 = 5
 
 if numero1 > numero2:
     print("El numero 1 es más grande y mejor que el anterior")
+elif numero1 == numero2:
+    print("son iguales")
+else:
+    print("El numero 2 es más grande")    
     
---
+++ Proyectos/piedra_papel_tijeras.py
@@ -0,0 +1,8 @@
+eleccion = "Papel"
+
+if eleccion == "Papel":
+    pass
+elif eleccion == "Piedra":
+    pass
+elif eleccion == "tijeras";
+    pass
