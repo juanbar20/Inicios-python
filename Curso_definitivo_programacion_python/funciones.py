5d03eaad2a226204e7effd7fbbdd074aae184b34
Juanbar20 <Juanbar20.jdb@gmail.com>
2023-02-14 19:52:50 -0500

    Iniciando pruebas de funciones

--- Curso_definitivo_programacion_python/funciones.py
+++ Curso_definitivo_programacion_python/funciones.py
@@ -1,16 +1,17 @@
 import os
 os.system("cls")
 
+def operaciones():
+    print(5+5)
+    print(10/2)
+    print(6+6)
+
 print("Aqui comienza mi programación")
 
-print(5+5)
-print(10/2)
-print(6+6)
+operaciones()
 
 print("Aqui hacemos otra cosa")
 
-print(5+5)
-print(10/2)
-print(6+6)
+operaciones()
 
 print("Fin de mi programa")
