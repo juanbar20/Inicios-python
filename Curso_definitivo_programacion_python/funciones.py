1353ad90675afc3b780a32e1789ef9ca1627e7a5
Juanbar20 <Juanbar20.jdb@gmail.com>
2023-02-14 19:44:31 -0500

    Inicio de uso de funciones

---
+++ Curso_definitivo_programacion_python/funciones.py
@@ -0,0 +1,16 @@
+import os
+os.system("cls")
+
+print("Aqui comienza mi programación")
+
+print(5+5)
+print(10/2)
+print(6+6)
+
+print("Aqui hacemos otra cosa")
+
+print(5+5)
+print(10/2)
+print(6+6)
+
+print("Fin de mi programa")
