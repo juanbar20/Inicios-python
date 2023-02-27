fe05827365aa31d5946a472a5bf54639767f2fbc
Juanbar20 <Juanbar20.jdb@gmail.com>
2023-02-27 17:22:05 -0500

    Viendo los diferentes tipos de lista y demas

---
+++ Curso_definitivo_programacion_python/diccionarios.py
@@ -0,0 +1,13 @@
+import os
+os.system("cls")
+
+
+#comida = {"banana", "cebolla", "nuez"}
+
+comida = {
+    "fruta": "banana",
+    "verdura": "cebolla",
+    "snack": "nuez"
+}
+
+print(comida["fruta"])
