1e01bbd751bd146324591c2b389fd51efae8328d
Juanbar20 <Juanbar20.jdb@gmail.com>
2023-02-26 23:36:01 -0500

    Creando el juego adivina el numero

---
+++ Proyectos/adivina_el_numero.py
@@ -0,0 +1,21 @@
+import os
+os.system("cls")
+
+import random
+
+numero = random.randint(1, 100)
+intentos = 5
+
+while(intentos > 0):
+    eleccion = int(input("Elige el numero: "))
+    if eleccion > numero:
+        print("Ell numero es mas pequeño")
+        intenos =- 1
+    elif eleccion < numero:
+        print("El numero es mas grande")
+        intenos =- 1
+    else:
+        print("Ganaste")
+        break
+
+print("Se acabaron los intentos")
