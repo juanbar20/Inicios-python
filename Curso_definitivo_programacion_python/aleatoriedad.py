ef3ab95254f260bfc63fc995f8345badc0d89c74
Juanbar20 <Juanbar20.jdb@gmail.com>
2023-02-26 23:18:24 -0500

    Usando funciones de la libreria

--- Curso_definitivo_programacion_python/aleatoriedad.py
+++ Curso_definitivo_programacion_python/aleatoriedad.py
@@ -1,1 +1,11 @@
+import os
+os.system("cls")
+
 import random
+
+#numero = random.randint(0, 10)
+#print(numero)
+
+texto = "Kazel es la mejor platforma de educación de internet"
+letra = random.choice(texto)
+print(letra)
