e5c8e5990c565475e219d788c8edb3a2a2fea154
Juanbar20 <juanbar20.jdb@gmail.com>
2023-02-28 01:53:00 -0500

    Creando el generador de contraseña

---
+++ Proyectos/generador_de_pasword.py
@@ -0,0 +1,10 @@
+import os
+os.system("cls")
+
+letras_minusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+letras_mayusculas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+simbolos = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '=', '<', '>', '?']
+
+caracteres = letras_mayusculas + letras_minusculas + numeros + simbolos
+print(caracteres)
