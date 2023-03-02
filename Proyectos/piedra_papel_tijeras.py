46e62d592310b6d3b42f65ecee37ba352c02fa39
Juanbar20 <juanbar20.jdb@gmail.com>
2023-03-02 00:33:43 -0500

    modificando el piedra papel o tijeras con random

--- Proyectos/piedra_papel_tijeras.py
+++ Proyectos/piedra_papel_tijeras.py
@@ -1,18 +1,15 @@
 import os
 os.system("cls")
 
+import random
 
 eleccion = input("Ingrese su opción perro hp: ")
 
-if eleccion == "Papel" or eleccion == "papel":
-    print("Computador elije Tijeras, gana computador :)")
-elif eleccion == "Piedra" or eleccion == "piedra":
-    print("Computadora elige Papel. Gana la computadora :) ")
-elif eleccion == "Tijeras" or eleccion == "tijeras":
-    print("Computadora elige Piedra. Gana la computadora :)")
-else:
-    print("Ingresa una opción correta")
+computador = 1
+
+while (computador != 4):
+    computador = random.randrange(1,4)
+
+    print(computador)
 
 
-variable = "5"
-print(f"El numero de incidentes es {variable}")
