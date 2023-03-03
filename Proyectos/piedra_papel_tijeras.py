1c575c2318bf6341417c146e4f3388c84c429452
Juanbar20 <juanbar20.jdb@gmail.com>
2023-03-02 23:29:27 -0500

    Mejora de piedra papel o tijeras

--- Proyectos/piedra_papel_tijeras.py
+++ Proyectos/piedra_papel_tijeras.py
@@ -3,13 +3,51 @@ os.system("cls")
 
 import random
 
-eleccion = input("Ingrese su opción perro hp: ")
+print("Wenas")
 
-computador = 1
+eleccion = int(input(" 1: Piedra \n 2: Papel \n 3: Tijeras   \nIngrese su opción perro hp: "))
 
-while (computador != 4):
-    computador = random.randrange(1,4)
 
-    print(computador)
+
+if eleccion ==  1:
+    eleccion = "Piedra"
+    operador_eleccion = 1
+elif eleccion == 2:
+    eleccion = "Papel"
+    operador_eleccion = 2
+elif eleccion ==  3:
+    eleccion = "Tijeras"
+    operador_eleccion = 3
+else:
+    print("Su eleccion no esta en las opciones dadas, por favor elija de nuevo")
+
+computador = random.randrange(1,4)
+
+if computador == 1:
+    computador = "Piedra"
+    operador_computador = 1
+elif computador == 2:
+    computador = "Papel"
+    operador_computador = 2
+else:
+    computador = "Tijeras"
+    operador_computador = 3
+
+
+
+while (operador_computador != 4):
+    
+    if eleccion == computador:
+        print(f"Elegiste {eleccion} y el computador saco {computador} es empate. ")
+        break
+    else:
+
+        if operador_eleccion == 1 :
+            pass
+        elif operador_eleccion == 2:
+            pass
+        else:
+            pass
+print(computador)
 
 
