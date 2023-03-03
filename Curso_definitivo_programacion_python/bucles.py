f61a75dce7fe3cf2d509f5f19dfc056c4380351d
Juanbar20 <juanbar20.jdb@gmail.com>
2023-03-03 03:01:49 -0500

    usos del while por ahora

--- Curso_definitivo_programacion_python/bucles.py
+++ Curso_definitivo_programacion_python/bucles.py
@@ -5,7 +5,9 @@ def run():
     potencia_2 = 2**contador
 
     while potencia_2 < LIMITE:
-        pass
+        print(f"2 elevado a {contador} es igual a: {potencia_2}")
+        contador += 1
+        potencia_2 = 2**contador
 
 if __name__ == '__main__':
     run()
