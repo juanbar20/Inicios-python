fbe5e3bfa3c41e17a1bfee6b47a3a090f8f661a8
Juanbar20 <juanbar20.jdb@gmail.com>
2023-03-04 02:05:41 -0500

    Haciendo la prueba de primalidad

--- Curso_definitivo_programacion_python/for.py
+++ Curso_definitivo_programacion_python/for.py
@@ -1,2 +1,12 @@
-for i in range(1,11):
-    print(i)
+#for i in range(1,11):
+#    print(i)
+
+def run():
+#    for contador in range (1001):
+#        print(contador)
+    for i in range(10):
+        print(11*i)
+
+
+if __name__ == "__main__":
+    run()
