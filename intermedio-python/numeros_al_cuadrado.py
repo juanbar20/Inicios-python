d939dea8a4b9a76086b8d461ea2d4167952adf34
Juanbar20 <Juanbar20.jdb@gmail.com>
2023-03-07 12:57:32 -0500

    Arreglando programa numeros al cuadrado

--- intermedio-python/numeros_al_cuadrado.py
+++ intermedio-python/numeros_al_cuadrado.py
@@ -2,11 +2,11 @@ import os
 os.system("cls")
 
 def run():
-    repeticiones = int(input("Ingresa hasta que numero quieres ver al cuadrado"))
+    repeticiones = int(input("Ingresa hasta que numero quieres ver al cuadrado: "))
 
     for i in range (1, repeticiones + 1):
         operacion = i ** 2
-        print(f"El numero {i} al cuadrado tiene como resultado {repeticiones}")
+        print(f"El numero {i} al cuadrado tiene como resultado {operacion} ")
 
 
 if __name__ == "__main__":
