bb94878d806d340d917b6ca4680ea682c6a1b4a7
Juanbar20 <Juanbar20.jdb@gmail.com>
2023-03-08 11:41:38 -0500

    probando dic comprehencions

--- intermedio-python/dic_comprehensions.py
+++ intermedio-python/dic_comprehensions.py
@@ -3,13 +3,16 @@ os.system("cls")
 
 
 def run():
-    my_dict = {}
+    """my_dict = {}
 
     for i in range(1, 101):
-        my_dict[i] = i ** 3
+        if i % 3 != 0:
+            my_dict[i] = i ** 3
 
-    print(my_dict)
+    print(my_dict)"""
 
+    my_dict = {i: i**3 for i in range(1, 101) if i % 3 != 0}
+    print (my_dict)
 
 if __name__ == "__main__":
     run()
