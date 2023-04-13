32e62df00a881415cc3e39e4598eb27d367ee581
Juanbar20 <Juanbar20.jdb@gmail.com>
2023-04-12 21:16:10 -0500

    Creando cookies

--- Flask/main.py
+++ Flask/main.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip',user_ip)
+
+    return response
+
+
+
 @app.route('/')
 def hello():
-    return "Platzi"
+    user_ip = request.remote_addr
+    return "Hello World Flask, su IP es {}".format(user_ip)
