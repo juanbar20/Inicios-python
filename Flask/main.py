1fd28420da11dc260c0e6228dccd6e6ec6f1eb7d
Juanbar20 <juanbar20.jdb@gmail.com>
2023-04-15 21:21:19 -0500

    Usando listas

--- Flask/main.py
+++ Flask/main.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
+
+todos = ['TODO 1', 'TODO 2', 'TODO 3']
 
 
 @app.route('/')
@@ -17,4 +19,9 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = request.cookies.get('user_ip')
-    return "Hello World Flask, su IP es {}".format(user_ip)
+    context = {
+        'user_ip' : user_ip,
+        'todo': todos,
+    }
+
+    return render_template('hello.html', **context, todos = todos)
